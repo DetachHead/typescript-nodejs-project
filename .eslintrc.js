179499374e37272b2586d0420fcc18455ab8439c
DetachHead <detachhead@users.noreply.github.com>
2021-07-20 21:25:17 +1000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-plugin-expect-type/recommended',
+    'prettier',
+    'plugin:eslint-comments/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'eslint-plugin-expect-type'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'never'],
+    'expect-type/expect': 'error',
+    '@typescript-eslint/ban-types': 'off',
+    'spaced-comment': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
+    'eslint-comments/no-unused-disable': 'error',
+    'eslint-comments/require-description': 'error',
+  },
+}
