f5825f5b14641303c39fc70da2748a548e196a34
detachhead <detachhead@users.noreply.github.com>
2021-12-02 09:14:27 +1000

    update eslint rules

--- src/__tests__/main.spec.ts
+++ src/__tests__/main.spec.ts
@@ -1,5 +1,5 @@
 import { PowerAssert } from 'typed-nodejs-assert'
-// eslint-disable-next-line @typescript-eslint/no-var-requires -- hack to fix type definitions for the nodejs assertion library.
+// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment -- hack to fix type definitions for the nodejs assertion library.
 const assert: PowerAssert = require('power-assert')
 
 test('main', () => {
