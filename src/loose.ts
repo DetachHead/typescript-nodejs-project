ab1f0e57d84c6af716bcc8eacaebf57b8c0de2b4
DetachHead <detachhead@users.noreply.github.com>
2022-05-23 16:23:48 +1000

    repro for language server issue

---
+++ src/loose.ts
@@ -0,0 +1,6 @@
+export {}
+
+//this is just here to prove its using the correct tsconfig
+function foo(bar) {} // no error
+
+pm //error
---
+++ src/strict.ts
@@ -0,0 +1,7 @@
+export {}
+
+//this is just here to prove its using the correct tsconfig
+function foo(bar) {} // error
+
+
+pm // no error, as the module is
